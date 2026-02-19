6166a9f1a4d38720acd477304390bfde80d7558a
Mateus Luiz <mateuslfreitasb@gmail.com>
2026-02-19 09:04:23 -0300

    Refactor enemy vayne behavior

--- Assets/Scripts/EnemyVayneBehavior.cs
+++ Assets/Scripts/EnemyVayneBehavior.cs
@@ -13,13 +13,17 @@ public class EnemyVayneBehavior : MonoBehaviour
 {
     public EnemyVayneState currentState;
     public GameObject target;
-
-    public float attackWaitTime = 2;
+    
     private float attackTimer;
-
-    public int rollDistance = 3;
     private float rolledDistance;
     private Vector2 rollTarget;
+
+    [SerializeField] private float attackWaitTime = 2;
+
+    [SerializeField] private int rollDistance = 3;
+    [SerializeField] private int rollSpeed = 5;
+    [SerializeField] private int followSpeed = 3;
+    [SerializeField] private int detectionRange = 4;
 
 
     void Start()
@@ -33,72 +37,12 @@ public class EnemyVayneBehavior : MonoBehaviour
     {
         if (!target) return;
 
-        if (currentState == EnemyVayneState.AttackTarget)
+        switch (currentState)
         {
-            attackTimer += Time.deltaTime;
-
-            if (attackTimer >= attackWaitTime)
-            {
-                currentState = EnemyVayneState.FollowTarget;
-                ResetStateMutation();
-            }
-
-            return;
-        }
-
-        if (currentState == EnemyVayneState.Rolling)
-        {
-            int speed = 5;
-            transform.position = Vector2.MoveTowards(transform.position, rollTarget, speed * Time.deltaTime);
-
-            rolledDistance = rolledDistance + (rollDistance * Time.deltaTime);
-
-            if (rolledDistance >= rollDistance)
-            {
-                currentState = EnemyVayneState.AttackTarget;
-            }
-
-            return;
-        }
-
-        if (currentState == EnemyVayneState.StartRoll)
-        {
-            Vector2 direction = (transform.position - target.transform.position).normalized;
-            
-            float rollRadians;
-            if (direction.x <= 0)
-            {
-                // if is left side of the target, goes right
-                rollRadians = Mathf.Deg2Rad * Random.Range(-25, 26);
-            } else
-            {
-                // if is right side of the target, goes left
-                rollRadians = Mathf.Deg2Rad * Random.Range(-205, -156);
-            }
-            
-            Vector2 distanceCoord = new Vector2(transform.position.x + rollDistance, transform.position.y);
-
-            rollTarget = new Vector2(
-                (distanceCoord.x * Mathf.Cos(rollRadians)) - (distanceCoord.y * Mathf.Sin(rollRadians)),
-                (distanceCoord.x * Mathf.Sin(rollRadians)) + (distanceCoord.y * Mathf.Cos(rollRadians))
-            );
-
-            currentState = EnemyVayneState.Rolling;
-
-            return;
-        }
-
-        if (Vector2.Distance(transform.position, target.transform.position) <= 4)
-        {
-            currentState = EnemyVayneState.StartRoll;
-            return;
-        }
-
-        if (currentState == EnemyVayneState.FollowTarget)
-        {
-            Vector2 targetPosition = target.transform.position;
-            int speed = 3;
-            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+            case EnemyVayneState.AttackTarget: AttackTarget(); break;
+            case EnemyVayneState.Rolling: PerformRolling(); break;
+            case EnemyVayneState.StartRoll: StartRoll(); break;
+            case EnemyVayneState.FollowTarget: FollowTarget(); break;
         }
     }
 
@@ -114,5 +58,61 @@ public class EnemyVayneBehavior : MonoBehaviour
     {
         attackTimer = 0;
         rolledDistance = 0;
+    }
+
+    private void AttackTarget()
+    {
+        attackTimer += Time.deltaTime;
+
+        if (attackTimer >= attackWaitTime)
+        {
+            currentState = EnemyVayneState.FollowTarget;
+            ResetStateMutation();
+        }
+    }
+    
+    private void PerformRolling()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, rollTarget, rollSpeed * Time.deltaTime);
+
+        rolledDistance = rolledDistance + (rollSpeed * Time.deltaTime);
+
+        if (rolledDistance >= rollDistance)
+        {
+            currentState = EnemyVayneState.AttackTarget;
+        }
+    }
+    
+    private void StartRoll()
+    {
+        Vector2 direction = (transform.position - target.transform.position).normalized;
+        
+        float rollRadians;
+        if (direction.x <= 0)
+        {
+            // if is left side of the target, goes right
+            rollRadians = Mathf.Deg2Rad * Random.Range(-25, 26);
+        } else
+        {
+            // if is right side of the target, goes left
+            rollRadians = Mathf.Deg2Rad * Random.Range(-205, -156);
+        }
+
+        Vector2 rollDirection = new Vector2(Mathf.Cos(rollRadians), Mathf.Sin(rollRadians));
+        rollTarget = (Vector2)transform.position + rollDirection * rollDistance;
+        
+        currentState = EnemyVayneState.Rolling;
+    }
+
+    private void FollowTarget()
+    {
+        if (Vector2.Distance(transform.position, target.transform.position) <= detectionRange)
+        {
+            currentState = EnemyVayneState.StartRoll;
+            return;
+        }
+
+        Vector2 targetPosition = target.transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, followSpeed * Time.deltaTime);
     }
 }
